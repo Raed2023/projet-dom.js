47698cbccc0c8046243c6c42cf72d4e5021d836c
Raed2023 <raedbenabdallah25@gamil.com>
2023-01-29 14:55:38 +0100

    cars lounge

---
+++ shop.js
@@ -0,0 +1,9 @@
+
+const lamborghiniQty = document.getElementById("lamborghini-qty");
+const lamborghiniTotal = document.getElementById("lamborghini-total");
+const ferrariQty = document.getElementById("ferrari-qty");
+const ferrariTotal = document.getElementById("ferrari-total");
+const mclarenQty = document.getElementById("mclaren-qty");
+const mclarenTotal = document.getElementById("mclaren-total");
+const grandTotal = document.getElementById("grand-total");
+
